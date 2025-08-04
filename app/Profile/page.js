c5819fb4a7ad3882ce7928579922d555a467b611
devaneyj3 <jordandevaney28@gmail.com>
2025-08-04 16:18:23 -0400

    update to next auth 5

--- app/Profile/page.js
+++ app/Profile/page.js
@@ -1,5 +1,13 @@
+"use client";
 import React from "react";
+import { useSession } from "next-auth/react";
 
 export default function Profile() {
-	return <div>Profile</div>;
+	const { data: session } = useSession();
+	return (
+		<div>
+			<p>Hello, {session?.user.name}</p>
+			<p>Email: {session?.user.email}</p>
+		</div>
+	);
 }
--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,27 +1,2 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
-import NextAuth from "next-auth";
-
-import GoogleProvider from "next-auth/providers/google";
-
-const prisma = new PrismaClient();
-
-export const authOptions = {
-	providers: [
-		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-		}),
-	],
-	adapter: PrismaAdapter(prisma),
-	secret: process.env.NEXTAUTH_SECRET,
-	// Session configuration for production
-	session: {
-		strategy: "database",
-		maxAge: 30 * 240 * 60, // 30 days
-	},
-};
-
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
+import { handlers } from "@/auth";
+export const { GET, POST } = handlers;
